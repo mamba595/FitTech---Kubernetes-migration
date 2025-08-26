6aeec2441ca1969c03a6095d62fcaaa6a2657da4
theDwarf595 <theDwarf595@gmail.com>
2025-08-26 18:04:06 +0200

    K12

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -274,6 +274,16 @@ resource "aws_iam_role_policy_attachment" "eks_irsa_role_policy_attachment" {
     role       = aws_iam_role.eks_irsa_ecr_role.name
 }
 
+data "aws_eks_cluster_auth" "eks" {
+    name = aws_eks_cluster.eks_cluster.name
+}
+
+provider "kubernetes" {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 resource "kubernetes_service_account" "ecr_sa" {
   metadata {
     name      = "ecr-sa"

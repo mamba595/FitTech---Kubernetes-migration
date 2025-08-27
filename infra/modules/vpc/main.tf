939fd741e4ae581f76a35add89ec04b943536362
theDwarf595 <theDwarf595@gmail.com>
2025-08-27 10:12:38 +0200

    K24

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -79,25 +79,6 @@ resource "aws_route_table_association" "public_associations" {
     route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "alb_sg" {
-    name   = "alb-sg"
-    vpc_id = aws_vpc.my_vpc.id
-
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_security_group" "rds_sg" {
     name   = "rds-sg"
     vpc_id = aws_vpc.my_vpc.id

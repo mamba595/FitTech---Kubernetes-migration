020e207f7310a928e6d5c41f1e031ef6b86b40eb
theDwarf595 <theDwarf595@gmail.com>
2025-08-28 09:55:34 +0200

    K38

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -168,7 +168,7 @@ resource "aws_eks_node_group" "default" {
         min_size     = 1
     }
 
-    instance_types = ["t3.micro"]
+    instance_types = ["t3.medium"]
     disk_size      = 20
 
     ami_type       = "AL2_x86_64"
